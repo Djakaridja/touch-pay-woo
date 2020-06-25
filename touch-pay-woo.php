9806ae8cd07b07f028360bfd94ac32377309a9f5
Djakaridja <djakaridja.dioubate@gmail.com>
2020-06-25 00:04:25 +0000

    Update

--- touch-pay-woo.php
+++ touch-pay-woo.php
@@ -18,7 +18,7 @@
  */
 
 if (! defined('ABSPATH') ) {
-    exit; // Exit if accessed directly.
+    exit; // Exit if accessed directly
 }
 
 if (! in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_option('active_plugins')))) return;
